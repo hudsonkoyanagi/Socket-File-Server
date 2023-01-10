9bc4a97d276947643214f307be85c1309520e927
hudsonkoyanagi <63019013+hudsonkoyanagi@users.noreply.github.com>
2023-01-10 02:00:48 -0500

    Update Runner.java

--- SocketFileServer/src/Runner.java
+++ SocketFileServer/src/Runner.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Runner {
     public static void main(String[] args) {
         try {
-            Thread server = new MockAPI(50000);
+            Thread server = new Server(50000);
             server.run();
 
         } catch (IOException e){
